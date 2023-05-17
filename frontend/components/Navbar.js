171f925022365761f753d963b140391d16310300
Ammar999aa <ammar.hm2012@gmail.com>
2023-05-16 23:35:29 -0700

    Navbar takes full width

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar({ handleTheme, getTheme, user }) {
     <div>
       <nav class="w-full fixed top-0 z-50">
 
-      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl sticky top-0 bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <div className="flex flex-wrap justify-between items-center mx-auto w-full sticky top-0 bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
             <NavLink to="/" className="site-title">
               <div className="grid gap-4 sm:grid-cols-2">
               <img
