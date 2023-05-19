e7f5c6159d57f3c1f70a88ed6c705f7fbfb52ad7
cookiemonster04 <cuileo2004@gmail.com>
2023-05-18 23:45:04 -0700

    debugging production socket url

--- frontend/socket.js
+++ frontend/socket.js
@@ -1,8 +1,10 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:8000';
+console.log("process.env", process.env.NODE_ENV);
+const URL =
+  process.env.NODE_ENV === "production" ? undefined : "http://localhost:8000";
 
 export const socket = io(URL, {
-    autoConnect: false
+  autoConnect: false,
 });
