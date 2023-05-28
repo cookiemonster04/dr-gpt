2cdfbb1b4892d5f643c5d5f8e528f22f4730c58e
Ammar999aa <ammar.hm2012@gmail.com>
2023-05-28 12:30:18 -0700

    Password requirements updated
    Fixed bug where the minimum characters is said to be 8, but is actually 9
    Passwords no longer require special characters.

---
+++ frontend/routes/ChatMenu.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import { socket } from "../socket";
+import axios from "axios";
+import { useNavigate, Navigate, useParams } from "react-router-dom";
+import ChatComp from "../components/ChatComp";
+import userId from Chat
+import Chat from "./Chat";
+
+const ChatMenu = ({ user }) => {
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default ChatMenu
