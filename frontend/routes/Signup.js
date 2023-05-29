ca51a7906ccaa76bc2aa0f1f0760273bb263cca1
cookiemonster04 <cuileo2004@gmail.com>
2023-05-28 19:02:18 -0700

    Require accepting ToS and PP at signup

--- frontend/routes/Signup.js
+++ frontend/routes/Signup.js
@@ -42,9 +42,15 @@ const Signup = ({ user, setUser, themeMode }) => {
   const [password, setPassword] = useState("");
   const [isError, setIsError] = useState(false);
   const [messages, setMessage] = useState("");
+  const [agreed, setAgreed] = useState(false);
   const [submitted, setSubmitted] = useState(false);
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (!agreed) {
+      setIsError(true);
+      setMessage("Please accept our Terms of Service and Privacy Policy to continue.");
+      return;
+    }
     axios
       .post("/api/user", {
         email: email,
@@ -112,13 +118,25 @@ const Signup = ({ user, setUser, themeMode }) => {
               setFormValue={setPassword}
               placeholder=""
             />
-            <input type="checkbox" className="mr-2">
-            </input>
+            {/* <input type="checkbox" className="mr-2">
+            </input> */}
 
-            
-            <label>
-              I have read and agree to Vitawise's <NavLink to="/terms" className="underline">Terms of Service</NavLink> and <NavLink to="/privacy" className="underline">Privacy Policy</NavLink>.
-            </label>
+            <div class="flex items-center">
+              <input
+                checked={agreed}
+                id="checked-checkbox"
+                type="checkbox"
+                value=""
+                // class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                onChange={() => setAgreed(!agreed)}
+              ></input>
+              <label
+                for="checked-checkbox"
+                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                I have read and agree to Vitawise's <NavLink to="/terms" className="underline">Terms of Service</NavLink> and <NavLink to="/privacy" className="underline">Privacy Policy</NavLink>.
+              </label>
+            </div>
             
             <label
               htmlFor="signup_submit"
